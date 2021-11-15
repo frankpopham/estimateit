e16a1e37edce12f8853697fe1c9d5fe422ed30fb
Frank Popham <frankpophamathome@gmail.com>
2021-11-15 16:07:27 +0000

    improvements to svycontrasts

--- R/estimateit.R
+++ R/estimateit.R
@@ -4,62 +4,70 @@
 #' @param data A data frame containing the outcome
 #' @param outcome A binary outcome variable
 #' @return Returns a summary table as a tibble, the model, and the individual effects
-#'    (X0 = "Control", X1="Treat", D=their difference, RR= their relative "risk",
-#'    OR=their odds ratio)
+#'    (Control, Treatment, D=their difference, logRR= their log relative "risk",
+#'    logOR=their log odds ratio). The individual effects are svrepstat objects that
+#'    can be further analysed using relevant survey functions. The table displays the exponential
+#'    of the log relative effects. In other words the relative risk and the odds ratio.
 #' @importFrom magrittr %>%
 #' @export
 #' @examples
 #' library(cobalt)
 #' library(WeightIt)
-#'
+#' data("lalonde", package = "cobalt")
 #' W1 <- weightit(treat ~ age + educ + married +
 #'                 re74, data = lalonde,
-#'               method = "ps", estimand = "ATT")
+#'               method = "ps", estimand = "ATE")
 #' summary(W1)
 #' bal.tab(W1)
 #' E1 <- estimateit(W1, nodegree, lalonde)
 #' E1
 
+
+
 estimateit <- function(weightitobj, outcome, data) {
   df <- dplyr::select(data, Y={{outcome}}) %>%
-    dplyr::bind_cols(X=weightitobj$treat) %>%
-    dplyr::bind_cols(w=weightitobj$weights) %>%
-    dplyr::mutate(X=ifelse(X==weightitobj$focal, 1, 0))
+    dplyr::bind_cols(treat=weightitobj$treat) %>%
+    dplyr::bind_cols(w=weightitobj$weights)
 
 out_model_ds <- survey::svydesign(id = ~1, data = df, weights = df$w)
 
-out_model <- survey::svyglm(Y ~ factor(X), family = binomial, design = out_model_ds)
+out_model <- survey::svyglm(Y ~ treat, family = binomial, design = out_model_ds)
 
-sum_model <- broom::tidy(out_model, conf.int = TRUE, exponentiate = TRUE) %>%
-    dplyr::filter(term == "factor(X)1") %>%
-    dplyr::mutate(Effect = "OR") %>%
-    dplyr::select(Effect, Estimate = estimate, Conf.low=conf.low, Conf.high=conf.high)
+out <- survey::svycontrast(out_model, list(X0l=c(1,0), X1l=c(1,1)))
 
 
-
-X0 <- survey::svycontrast(out_model, quote(exp(`(Intercept)`) / (1 + exp(`(Intercept)`))))
-X1 <- survey::svycontrast(out_model, quote((exp(`(Intercept)` + `factor(X)1`))
-    / (1 + (exp(`(Intercept)` + `factor(X)1`)))))
-rr <- survey::svycontrast(out_model, quote(log((exp(`(Intercept)` + `factor(X)1`))
-    / (1 + (exp(`(Intercept)` + `factor(X)1`)))) -
-      log((exp(`(Intercept)`) / (1 + exp(`(Intercept)`))))))
-rd <- survey::svycontrast(out_model, quote(((exp(`(Intercept)` + `factor(X)1`))
-    / (1 + (exp(`(Intercept)` + `factor(X)1`)))) -
-      (exp(`(Intercept)`) / (1 + exp(`(Intercept)`)))))
+logOR <- survey::svycontrast(out, quote(`X1l` - `X0l`))
+Control <- survey::svycontrast(out, quote(exp(`X0l`) / (1 + (exp(`X0l`)))))
+Treated <- survey::svycontrast(out, quote(exp(`X1l`) / (1 + (exp(`X1l`)))))
+logRR <- survey::svycontrast(out, quote(log(exp(`X1l`) / (1 + (exp(`X1l`)))) -
+                                     log(exp(`X0l`) / (1 + (exp(`X0l`))))))
+D <- survey::svycontrast(out, quote(exp(`X1l`) / (1 + (exp(`X1l`))) -
+                                     exp(`X0l`) / (1 + (exp(`X0l`)))))
 
 
-summary <- list(X0 = X0, X1 = X1, D = rd, RR = rr)
 
-contrasts_ci <- purrr::map(summary, confint) %>%
-    purrr::map_dfr(~ tibble::as_tibble(.x), .id = "Effect")
 
-contrasts <- purrr::map_dfr(summary, ~ tibble::as_tibble(.x), .id="Effect") %>%
-  dplyr::bind_cols(dplyr::select(contrasts_ci, -Effect)) %>%
-  dplyr::select(Effect, Estimate = nlcon, Conf.low = "2.5 %", Conf.high = "97.5 %") %>%
-  dplyr::mutate(dplyr::across(c(Estimate, Conf.low, Conf.high),
-                              ~ dplyr::if_else(Effect == "RR", exp(.x), .x))) %>%
-  dplyr::bind_rows(sum_model) %>%
-  dplyr::mutate(Estimand = weightitobj$estimand) %>%
-  dplyr::select(Estimand, tidyselect::everything())
-return(list(Outcome_model=out_model, X0 = X0, X1 = X1, D = rd, RR = rr, Table=contrasts))
+#no tidy for svyrep - this based on https://www.tidymodels.org/learn/develop/broom/
+tidyup <- function(x, conf.int = TRUE, conf.level = 0.95) {
+   result <- tibble::as_tibble(x) %>%
+     dplyr::select(-SE)
+     colnames(result) <- c("estimate")
+  if (conf.int) {
+    ci <- tibble::as_tibble(confint(x, level = conf.level))
+    colnames(ci) <- c("conf.low", "conf.high")
+    result <- dplyr::bind_cols(result, ci)
+  }
+result
+}
+
+summarylist <- list(Control=Control, Treated=Treated, D=D, RR = logRR, OR=logOR)
+
+contrasts <- purrr::map_dfr(summarylist, ~ tidyup(.x), .id="effect") %>%
+  dplyr::mutate(dplyr::across(c(estimate, conf.low, conf.high),
+                              ~ dplyr::if_else(effect == "RR" | effect =="OR", exp(.x), .x))) %>%
+  dplyr::mutate(estimand = weightitobj$estimand) %>%
+  dplyr::select(estimand, tidyselect::everything())
+
+return(list(Outcome_model=out_model, Control=Control, Treated=Treated, D=D,
+            logRR=logRR, logOR=logOR, Table=contrasts))
 }
--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,6 @@
+
 utils::globalVariables(c("X", "Conf.high", "Conf.low",  "Effect",  "Estimand",  "Estimate", "binomial",
-                         "conf.high", "conf.low", "confint", "estimate", "nlcon", "term))
+                         "conf.high", "conf.low", "confint", "estimate", "nlcon", "term", "SE", "estimand"))
+
+
+
